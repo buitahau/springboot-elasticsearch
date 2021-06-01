4733e48e4318564aca27a8278502b028e7ca3d62
ces-hau-bui <hau.bui@codeenginestudio.com>
2021-06-01 22:12:14 +0700

    Add configuration to connect to the Elasticsearch server

---
+++ src/main/java/com/hau/kute/springbootelasticsearch/config/ElasticsearchClientConfig.java
@@ -0,0 +1,27 @@
+package com.hau.kute.springbootelasticsearch.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.hau.kute.springbootelasticsearch.repositories")
+@ComponentScan(basePackages = "com.hau.kute.springbootelasticsearch")
+public class ElasticsearchClientConfig extends AbstractElasticsearchConfiguration {
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration =
+                ClientConfiguration
+                        .builder()
+                        .connectedTo("localhost:9200")
+                        .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
